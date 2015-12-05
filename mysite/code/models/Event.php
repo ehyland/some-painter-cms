dbd726ef8e53923c7c30b9853d96d313579bdd5a
Eamon Hyland <ehyland90@gmail.com>
2015-12-05 12:17:31 +0000

    Fixed indentation

--- mysite/code/models/Event.php
+++ mysite/code/models/Event.php
@@ -1,34 +1,34 @@
 <?php
 class Event extends DataObject{
 
-  private static $db = array(
-    'Title' => 'Varchar(255)',
-    'ArtistName' => 'Varchar(255)',
-    'StartDate' => 'SS_Datetime',
-    'EndDate' => 'SS_Datetime'
-  );
+    private static $db = array(
+        'Title' => 'Varchar(255)',
+        'ArtistName' => 'Varchar(255)',
+        'StartDate' => 'SS_Datetime',
+        'EndDate' => 'SS_Datetime'
+    );
 
-  private static $has_one = array(
-    'Gallery' => 'Gallery',
-    'GoogleCalendarEvent' => 'GoogleCalendarEvent'
-  );
+    private static $has_one = array(
+        'Gallery' => 'Gallery',
+        'GoogleCalendarEvent' => 'GoogleCalendarEvent'
+    );
 
-  public static function create_or_update_with_calendar_data($data){
+    public static function create_or_update_with_calendar_data($data){
 
-    // Build update map
-    $update = $data['derived'];
-    $update['GoogleCalendarEventID'] = $data['GoogleCalendarEventID'];
-    $update['Title'] = $data['calendar']['summary'];
+        // Build update map
+        $update = $data['derived'];
+        $update['GoogleCalendarEventID'] = $data['GoogleCalendarEventID'];
+        $update['Title'] = $data['calendar']['summary'];
 
-    // Get or create event
-    $filter = array('GoogleCalendarEventID' => $data['GoogleCalendarEventID']);
-    if (!$event = self::get()->filter($filter)->first()) {
-        $event = self::create();
+        // Get or create event
+        $filter = array('GoogleCalendarEventID' => $data['GoogleCalendarEventID']);
+        if (!$event = self::get()->filter($filter)->first()) {
+            $event = self::create();
+        }
+
+        // Update event
+        return $event->update($update)->write();
     }
-
-    // Update event
-    return $event->update($update)->write();
-  }
 
     public function forAPI(){
         $data = $this->getBaseAPIFields(array(
